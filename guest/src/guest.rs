6e7b2dbf5577ea087696ba920b1a9c27aa9d380d
David Meister <thedavidmeister@gmail.com>
2020-01-30 22:15:00 +0100

    wip on data passing

--- guest/src/guest.rs
+++ guest/src/guest.rs
@@ -3,42 +3,48 @@ extern crate wee_alloc;
 use std::mem;
 use std::slice;
 use common::bits_n_pieces::u64_merge_bits;
-use common::memory::StringPtr;
-use common::memory::StringCap;
-use common::memory::PtrLen;
-use common::bits_n_pieces::u64_split_bits;
+// use common::memory::StringPtr;
+// use common::memory::StringCap;
+// use common::memory::PtrLen;
+// use common::bits_n_pieces::u64_split_bits;
 
 extern "C" {
-    fn host_process_string(ptr: StringPtr, cap: StringCap) -> PtrLen;
+    // fn host_process_string(ptr: StringPtr, cap: StringCap) -> PtrLen;
 }
 
 // Use `wee_alloc` as the global allocator.
 #[global_allocator]
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
-fn mem_string(ptr: StringPtr, len: StringCap) -> String {
-    unsafe { std::str::from_utf8_unchecked(slice::from_raw_parts(ptr as _, len as _)) }.into()
+pub fn host_string(ptr: u32, len: u32) -> String {
+    let slice: &[u8] = unsafe { slice::from_raw_parts(ptr as _, len as _) };
+    std::str::from_utf8(slice).unwrap().into()
+    // String::from_raw_parts(ptr, cap as _, cap as _)
 }
 
 #[no_mangle]
 /// hack to allow the host to allocate a string capacity and receive the pointer to write into it
 /// from outside
-pub extern "C" fn pre_alloc_string(cap: StringCap) -> StringPtr {
+pub extern "C" fn pre_alloc_string(cap: u32) -> i32 {
     // https://doc.rust-lang.org/std/string/struct.String.html#examples-8
     // Prevent automatically dropping the String's data
-    mem::ManuallyDrop::new(Vec::with_capacity(cap as _)).as_ptr()
+    let dummy:Vec<u8> = Vec::with_capacity(cap as _);
+    let ptr = dummy.as_slice().as_ptr() as i32;
+    mem::ManuallyDrop::new(dummy);
+    // mem::forget(dummy);
+    ptr
 }
 
 #[no_mangle]
-pub extern "C" fn process_string(ptr: StringPtr, cap: StringCap) -> PtrLen {
+pub extern "C" fn process_string(ptr: u32, cap: u32) -> u64 {
     // get the string the host is trying to pass us out of memory
     // the ptr and cap line up with what was previously allocated with pre_alloc_string
-    let s = mem_string(ptr, cap);
-
+    let s = host_string(ptr, cap);
 
     // imported host function calls are always unsafe
-    let (processed_ptr, processed_len) = u64_split_bits(unsafe { host_process_string(s.as_ptr(), s.len()) });
-    let host_processed_string = mem_string(processed_ptr as _, processed_len as _);
-    let guest_processed = format!("guest: {}", host_processed_string);
+    // let (processed_ptr, processed_len) = u64_split_bits(unsafe { host_process_string(s.as_ptr(), s.len()) });
+    // let host_processed_string = host_string(processed_ptr as _, processed_len as _);
+    let guest_processed = format!("guest: {}", s);
     u64_merge_bits(guest_processed.as_ptr() as _, guest_processed.len() as _)
+    // u64_merge_bits(ptr, cap)
 }
--- host/src/host.rs
+++ host/src/host.rs
@@ -4,7 +4,8 @@ use common::memory::PtrLen;
 use std::fs::File;
 use std::io::prelude::*;
 use wasmer_runtime::Ctx;
-use wasmer_runtime::Memory;
+use wasmer_runtime::Value;
+use wasmer_runtime::Instance;
 
 // Import the wasmer runtime so we can use it
 use wasmer_runtime::{func, imports, ImportObject};
@@ -36,7 +37,28 @@ pub fn import_object() -> ImportObject {
     }
 }
 
-fn write_mem_string(memory: &Memory, ptr: i32, len: i32) -> String {
+pub fn write_guest_string(instance: &mut Instance, s: String) -> (i32, i32) {
+    let guest_ptr = match instance
+        .call("pre_alloc_string", &[Value::I32(s.len() as _)])
+        .expect("run pre alloc")[0]
+    {
+        Value::I32(i) => i,
+        _ => unreachable!(),
+    };
+
+    let memory = instance.context_mut().memory(0);
+
+    for (byte, cell) in s
+        .bytes()
+        .zip(memory.view()[guest_ptr as _..(guest_ptr + s.len() as i32) as _].iter()) {
+            cell.set(byte)
+    }
+
+    (guest_ptr, s.len() as i32)
+}
+
+fn read_guest_string(ctx: &Ctx, ptr: i32, len: i32) -> String {
+    let memory = ctx.memory(0);
     let str_vec: Vec<_> = memory.view()[ptr as usize..(ptr + len) as usize]
         .iter()
         .map(|cell| cell.get())
@@ -47,95 +69,34 @@ fn write_mem_string(memory: &Memory, ptr: i32, len: i32) -> String {
 }
 
 fn host_process_string(ctx: &mut Ctx, ptr: i32, cap: i32) -> PtrLen {
-    let memory = ctx.memory(0);
-    let guest_string = write_mem_string(&memory, ptr, cap);
+    let guest_string = read_guest_string(ctx, ptr, cap);
     let processed_string = format!("host: {}", guest_string);
     u64_merge_bits(processed_string.as_ptr() as _, processed_string.len() as _)
 }
-
-// pub fn main() -> error::Result<()> {
-//     // Now that we have the wasm file as bytes, let's run it with the wasmer runtime
-//
-//     // Our import object, that allows exposing functions to our wasm module.
-//     // We're not importing anything, so make an empty import object.
-//     let import_object = imports! {
-//         "env" => {
-//             "host_process_string" => func!(host_process_string),
-//         },
-//     };
-//
-//     // Let's create an instance of wasm module running in the wasmer-runtime
-//     let instance = instantiate(&wasm_vec, &import_object)?;
-//
-//     // Lets get the context and memory of our Wasm Instance
-//     let wasm_instance_context = instance.context();
-//     let wasm_instance_memory = wasm_instance_context.memory(0);
-//
-//     // Let's get the pointer to the buffer defined by the wasm module in the wasm memory.
-//     // We use the type system and the power of generics to get a function we can call
-//     // directly with a type signature of no arguments and returning a WasmPtr<u8, Array>
-//     let get_wasm_memory_buffer_pointer: Func<(), WasmPtr<u8, Array>> = instance
-//         .func("get_wasm_memory_buffer_pointer")
-//         .expect("get_wasm_memory_buffer_pointer");
-//     let wasm_buffer_pointer = get_wasm_memory_buffer_pointer.call().unwrap();
-//
-//     // Let's write a string to the wasm memory
-//     let original_string = "Did you know";
-//     println!("The original string is: {}", original_string);
-//     // We deref our WasmPtr to get a &[Cell<u8>]
-//     let memory_writer = wasm_buffer_pointer
-//         .deref(wasm_instance_memory, 0, original_string.len() as u32)
-//         .unwrap();
-//     for (i, b) in original_string.bytes().enumerate() {
-//         memory_writer[i].set(b);
-//     }
-//
-//     // Let's call the exported function that concatenates a phrase to our string.
-//     let add_wasm_is_cool: Func<u32, u32> = instance
-//         .func("add_wasm_is_cool")
-//         .expect("Wasm is cool export");
-//     let new_string_length = add_wasm_is_cool.call(original_string.len() as u32).unwrap();
-//
-//     // Get our pointer again, since memory may have shifted around
-//     let new_wasm_buffer_pointer = get_wasm_memory_buffer_pointer.call().unwrap();
-//
-//     // Read the string from that new pointer.
-//     let new_string = new_wasm_buffer_pointer
-//         .get_utf8_string(wasm_instance_memory, new_string_length)
-//         .unwrap();
-//     println!("The new string is: {}", new_string);
-//
-//     // Asserting that the returned value from the function is our expected value.
-//     assert_eq!(new_string, "Did you know Wasm is cool!");
-//
-//     // Log a success message
-//     println!("Success!");
-//
-//     // Return OK since everything executed successfully!
-//     Ok(())
-// }
 
 #[cfg(test)]
 pub mod tests {
 
     use super::*;
     use wasmer_runtime::instantiate;
-    use wasmer_runtime::Value;
+    use common::bits_n_pieces::u64_split_bits;
+    use std::convert::TryInto;
 
     #[test]
     fn do_it() {
         let mut instance = instantiate(&load_wasm(), &import_object()).expect("build instance");
-        let starter_string = String::from("foo");
+        let starter_string = String::from("foobar");
 
-        let start_string_guest_ptr = match instance
-            .call("pre_alloc_string", &[Value::I32(starter_string.len() as _)])
-            .expect("run pre alloc")[0]
-        {
-            Value::I32(i) => i,
+        let (guest_ptr, guest_len) = write_guest_string(&mut instance, starter_string.clone());
+        println!("{} {}", guest_ptr, guest_len);
+
+        let (result_ptr, result_len) = u64_split_bits(match instance.call("process_string", &[Value::I32(guest_ptr), Value::I32(guest_len)]).expect("call error")[0] {
+            Value::I64(i) => i as u64,
             _ => unreachable!(),
-        };
+        });
+        println!("{} {}", result_ptr, result_len);
 
-        let memory = instance.context_mut().memory(0);
-        write_mem_string(memory, start_string_guest_ptr, starter_string.len() as _);
+        let result_string = read_guest_string(&instance.context(), result_ptr.try_into().unwrap(), result_len.try_into().unwrap());
+        println!("{}", result_string);
     }
 }
