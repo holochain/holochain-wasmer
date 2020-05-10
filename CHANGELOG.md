9c79b5d57279a78f67d2405903ef42873bfc6318
David Meister <thedavidmeister@gmail.com>
2020-05-11 00:09:34 +0400

    Release v0.0.30

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,20 @@
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 This project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.0.30] - 2020-05-10
+
+### Added
+
+### Changed
+
+### Deprecated
+
+### Removed
+
+### Fixed
+
+### Security
+
 ## [0.0.29] - 2020-05-02
 
 ### Added
--- config.nix
+++ config.nix
@@ -63,13 +63,13 @@ echo "All finished!!!"
   # the commit hash that the release process should target
   # this will always be behind what ends up being deployed
   # the release process needs to add some commits for changelog etc.
-  commit = "15d76c1e69fc78f605342b557a581f38510499bd";
+  commit = "35be4efaf2addc600d7307ec5d1dd4900a011f66";
 
   # the semver for prev and current releases
   # the previous version will be scanned/bumped by release scripts
   # the current version is what the release scripts bump *to*
   version = {
-   current = "0.0.29";
+   current = "0.0.30";
    previous = "_._._";
   };
 
