ec29dbb0122db9f09d3e087cd72a01732e9a43b1
Michael Dougherty <maackle.d@gmail.com>
2020-08-04 17:59:39 -0700

    Actually, nevermind on that last commit

--- crates/guest/src/guest.rs
+++ crates/guest/src/guest.rs
@@ -192,7 +192,6 @@ macro_rules! try_result {
     ( $e:expr, $fail:expr ) => {{
         match $e {
             Ok(v) => v,
-            Err(()) => $crate::ret_err!($fail),
             Err(e) => $crate::ret_err!(format!("{}: {:?}", $fail, e)),
         }
     }};
