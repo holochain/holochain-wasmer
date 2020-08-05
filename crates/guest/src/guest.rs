7a796b02006f2ddb38d7d07f1ba3f317c6c9636d
Michael Dougherty <maackle.d@gmail.com>
2020-08-04 17:59:01 -0700

    No extra context for unit errors

--- crates/guest/src/guest.rs
+++ crates/guest/src/guest.rs
@@ -192,6 +192,7 @@ macro_rules! try_result {
     ( $e:expr, $fail:expr ) => {{
         match $e {
             Ok(v) => v,
+            Err(()) => $crate::ret_err!($fail),
             Err(e) => $crate::ret_err!(format!("{}: {:?}", $fail, e)),
         }
     }};
