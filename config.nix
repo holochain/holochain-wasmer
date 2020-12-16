82fcbd006c40ed7b18b39da25aee85144bda71f0
freesig <tomrgowan@gmail.com>
2020-12-17 08:36:56 +1100

    release prep

--- config.nix
+++ config.nix
@@ -63,13 +63,13 @@ echo "All finished!!!"
   # the commit hash that the release process should target
   # this will always be behind what ends up being deployed
   # the release process needs to add some commits for changelog etc.
-  commit = "75fbb38610675c30f22a8cdc76eab32ff8b19d09";
+  commit = "ddc407c1103967a6bac1e80c9c13560f0dfb11af";
 
   # the semver for prev and current releases
   # the previous version will be scanned/bumped by release scripts
   # the current version is what the release scripts bump *to*
   version = {
-   current = "0.0.50";
+   current = "0.0.51";
    previous = "_._._";
   };
 
