a309b0950467b142aefccf21ee351b9aecd40c38
David Meister <thedavidmeister@gmail.com>
2021-01-06 22:16:24 +0400

    lint

--- crates/host/src/guest.rs
+++ crates/host/src/guest.rs
@@ -134,9 +134,6 @@ pub fn read_bytes(ctx: &Ctx, guest_ptr: GuestPtr) -> Result<Vec<u8>, WasmError> 
         .iter();
 
     let mut len_array = [0; std::mem::size_of::<Len>()];
-    // for i in 0..std::mem::size_of::<Len>() {
-    //     len_array[i] = len_iter.next().ok_or(WasmError::Memory)?.get();
-    // }
     for item in len_array.iter_mut().take(std::mem::size_of::<Len>()) {
         *item = len_iter.next().ok_or(WasmError::Memory)?.get();
     }
