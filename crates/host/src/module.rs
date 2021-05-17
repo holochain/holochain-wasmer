009ad4500348e9bc640c4bf41c0973566bb807f0
thedavidmeister <thedavidmeister@gmail.com>
2021-05-17 16:16:44 +0400

    lint

--- crates/host/src/module.rs
+++ crates/host/src/module.rs
@@ -3,6 +3,7 @@ use holochain_wasmer_common::WasmError;
 use once_cell::sync::Lazy;
 use parking_lot::Mutex;
 use parking_lot::RwLock;
+use rand::prelude::*;
 use std::collections::HashMap;
 use std::sync::Arc;
 use wasmer::Instance;
@@ -10,7 +11,6 @@ use wasmer::Module;
 use wasmer::Store;
 use wasmer::JIT;
 use wasmer_compiler_singlepass::Singlepass;
-use rand::prelude::*;
 
 pub struct ModuleCache(HashMap<[u8; 32], Arc<Module>>);
 pub struct InstanceCache(HashMap<[u8; 32], Arc<Mutex<Instance>>>);
@@ -22,21 +22,19 @@ impl ModuleCache {
         Self(HashMap::new())
     }
 
-    fn get_with_build_cache(&mut self, key: [u8; 32], wasm: &[u8]) -> Result<Arc<Module>, WasmError> {
+    fn get_with_build_cache(
+        &mut self,
+        key: [u8; 32],
+        wasm: &[u8],
+    ) -> Result<Arc<Module>, WasmError> {
         let store = Store::new(&JIT::new(Singlepass::new()).engine());
-        let module = Module::from_binary(&store, wasm).map_err(|e| WasmError::Compile(e.to_string()))?;
-        self.0.insert(
-            key,
-            Arc::new(module)
-        );
+        let module =
+            Module::from_binary(&store, wasm).map_err(|e| WasmError::Compile(e.to_string()))?;
+        self.0.insert(key, Arc::new(module));
         self.get(key, wasm)
     }
 
-    pub fn get(
-        &mut self,
-        key: [u8; 32],
-        wasm: &[u8]
-    ) -> Result<Arc<Module>, WasmError> {
+    pub fn get(&mut self, key: [u8; 32], wasm: &[u8]) -> Result<Arc<Module>, WasmError> {
         let mut rng = rand::thread_rng();
         if rng.gen::<u8>() == 0 {
             let _ = self.0.remove(&key);
