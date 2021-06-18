88870589bfc95c50047ab9d7cb29a25fc9e2c8c3
freesig <tomrgowan@gmail.com>
2021-06-19 01:30:53 +1000

    serialize module

--- crates/host/src/module.rs
+++ crates/host/src/module.rs
@@ -9,28 +9,31 @@ use wasmer::Store;
 use wasmer::Universal;
 
 #[derive(Default)]
-pub struct ModuleCache(HashMap<[u8; 32], Arc<Module>>);
+pub struct ModuleCache(HashMap<[u8; 32], Vec<u8>>);
 
 pub static MODULE_CACHE: Lazy<RwLock<ModuleCache>> =
     Lazy::new(|| RwLock::new(ModuleCache::default()));
 
 impl ModuleCache {
-    fn get_with_build_cache(
-        &mut self,
-        key: [u8; 32],
-        wasm: &[u8],
-    ) -> Result<Arc<Module>, WasmError> {
+    fn get_with_build_cache(&mut self, key: [u8; 32], wasm: &[u8]) -> Result<(), WasmError> {
         let store = Store::new(&Universal::new(Cranelift::default()).engine());
         let module =
             Module::from_binary(&store, wasm).map_err(|e| WasmError::Compile(e.to_string()))?;
-        self.0.insert(key, Arc::new(module));
-        self.get(key, wasm)
+        let module = module.serialize().unwrap();
+        self.0.insert(key, module);
+        Ok(())
     }
 
     pub fn get(&mut self, key: [u8; 32], wasm: &[u8]) -> Result<Arc<Module>, WasmError> {
-        match self.0.get(&key) {
-            Some(module) => Ok(Arc::clone(module)),
-            None => self.get_with_build_cache(key, wasm),
-        }
+        let module = match self.0.get(&key) {
+            Some(module) => module,
+            None => {
+                self.get_with_build_cache(key, wasm)?;
+                self.0.get(&key).expect("Was just inserted")
+            }
+        };
+        let store = Store::new(&Universal::new(Cranelift::default()).engine());
+        let module = unsafe { Module::deserialize(&store, module) };
+        Ok(Arc::new(module.unwrap()))
     }
 }
