c513880a335ee0ef7db05b82696fc1b72561601b
thedavidmeister <thedavidmeister@gmail.com>
2021-06-26 13:10:20 +0400

    plru cache trait

--- crates/host/src/module.rs
+++ crates/host/src/module.rs
@@ -14,47 +14,72 @@ use wasmer::Module;
 use wasmer::Store;
 use wasmer::Universal;
 
-pub type ModuleCacheKey = [u8; 32];
+pub type CacheKey = [u8; 32];
+pub type PlruKeyMap = BiMap<usize, CacheKey>;
 pub type SerializedModule = Vec<u8>;
+
+trait PlruCache {
+    type Item;
+
+    fn plru_mut(&mut self) -> &mut MicroCache;
+    fn key_map(&self) -> &PlruKeyMap;
+    fn key_map_mut(&mut self) -> &mut PlruKeyMap;
+    fn cache_mut(&mut self) -> &mut HashMap<CacheKey, Self::Item>;
+
+    fn put(&mut self, key: CacheKey, serialized_module: Self::Item) {
+        let plru_key = self.plru_mut().replace();
+        // if there is something in the cache for this plru slot already drop it.
+        if let Some(stale_key) = self.key_map().get_by_left(&plru_key).cloned() {
+            self.cache_mut().remove(&stale_key);
+        }
+        self.cache_mut().insert(key, serialized_module);
+        self.plru_mut().touch(plru_key);
+        self.key_map_mut().insert(plru_key, key);
+    }
+
+    fn touch(&mut self, key: CacheKey) {
+        let plru_key = *self
+            .key_map()
+            .get_by_right(&key)
+            // It is a bug to call touch on a cache MISS.
+            // It is also a bug if a cache HIT does not map to a plru key.
+            .expect("Missing serialized cache plru key mapping. This is a bug.");
+        self.plru_mut().touch(plru_key);
+    }
+}
 
 #[derive(Default)]
 pub struct SerializedModuleCache {
     plru: MicroCache,
-    key_map: BiMap<usize, ModuleCacheKey>,
-    cache: HashMap<ModuleCacheKey, SerializedModule>,
+    key_map: PlruKeyMap,
+    cache: HashMap<CacheKey, SerializedModule>,
 }
 
 pub static SERIALIZED_MODULE_CACHE: Lazy<RwLock<SerializedModuleCache>> =
     Lazy::new(|| RwLock::new(SerializedModuleCache::default()));
 
+impl PlruCache for SerializedModuleCache {
+    type Item = SerializedModule;
+
+    fn plru_mut(&mut self) -> &mut MicroCache {
+        &mut self.plru
+    }
+
+    fn key_map_mut(&mut self) -> &mut PlruKeyMap {
+        &mut self.key_map
+    }
+
+    fn key_map(&self) -> &PlruKeyMap {
+        &self.key_map
+    }
+
+    fn cache_mut(&mut self) -> &mut HashMap<CacheKey, Self::Item> {
+        &mut self.cache
+    }
+}
+
 impl SerializedModuleCache {
-    fn put(&mut self, key: ModuleCacheKey, serialized_module: SerializedModule) {
-        let plru_key = self.plru.replace();
-        // if there is something in the cache in this plru slot already drop it.
-        if let Some(stale_key) = self.key_map.get_by_left(&plru_key) {
-            self.cache.remove(stale_key);
-        }
-        self.cache.insert(key, serialized_module);
-        self.plru.touch(plru_key);
-        self.key_map.insert(plru_key, key);
-    }
-
-    fn touch(&mut self, key: ModuleCacheKey) {
-        self.plru.touch(
-            *self
-                .key_map
-                .get_by_right(&key)
-                // It is a bug to call touch on a cache MISS.
-                // It is also a bug if a cache HIT does not map to a plru key.
-                .expect("Missing serialized cache plru key mapping. This is a bug."),
-        )
-    }
-
-    fn get_with_build_cache(
-        &mut self,
-        key: ModuleCacheKey,
-        wasm: &[u8],
-    ) -> Result<Module, WasmError> {
+    fn get_with_build_cache(&mut self, key: CacheKey, wasm: &[u8]) -> Result<Module, WasmError> {
         let store = Store::new(&Universal::new(Cranelift::default()).engine());
         let module =
             Module::from_binary(&store, wasm).map_err(|e| WasmError::Compile(e.to_string()))?;
@@ -65,7 +90,7 @@ impl SerializedModuleCache {
         Ok(module)
     }
 
-    pub fn get(&mut self, key: ModuleCacheKey, wasm: &[u8]) -> Result<Module, WasmError> {
+    pub fn get(&mut self, key: CacheKey, wasm: &[u8]) -> Result<Module, WasmError> {
         match self.cache.get(&key) {
             Some(serialized_module) => {
                 let store = Store::new(&Universal::new(Cranelift::default()).engine());
@@ -80,7 +105,7 @@ impl SerializedModuleCache {
 }
 
 #[derive(Default)]
-pub struct ModuleCache(HashMap<ModuleCacheKey, Arc<Module>>, bool, AtomicUsize);
+pub struct ModuleCache(HashMap<CacheKey, Arc<Module>>, bool, AtomicUsize);
 
 pub static MODULE_CACHE: Lazy<RwLock<ModuleCache>> =
     Lazy::new(|| RwLock::new(ModuleCache::default()));
@@ -88,7 +113,7 @@ pub static MODULE_CACHE: Lazy<RwLock<ModuleCache>> =
 impl ModuleCache {
     fn get_with_build_cache(
         &mut self,
-        key: ModuleCacheKey,
+        key: CacheKey,
         wasm: &[u8],
     ) -> Result<Arc<Module>, WasmError> {
         let module = SERIALIZED_MODULE_CACHE.write().get(key, wasm)?;
@@ -101,7 +126,7 @@ impl ModuleCache {
         self.2.store(0, std::sync::atomic::Ordering::SeqCst);
     }
 
-    pub fn get(&mut self, key: ModuleCacheKey, wasm: &[u8]) -> Result<Arc<Module>, WasmError> {
+    pub fn get(&mut self, key: CacheKey, wasm: &[u8]) -> Result<Arc<Module>, WasmError> {
         let count = self.2.fetch_add(1, std::sync::atomic::Ordering::SeqCst);
         if count > 100 {
             let current = self.0.remove(&key);
