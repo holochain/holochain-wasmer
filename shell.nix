1892465bec7873be6cbbba9519563b6ae3ffc959
Stefan Junker <mail@stefanjunker.de>
2021-12-28 23:32:21 +0100

    use niv for nix dependencies

--- shell.nix
+++ shell.nix
@@ -1,11 +1,10 @@
 let
-  holonixPath = builtins.fetchTarball {
-    url = "https://github.com/holochain/holonix/archive/2b7797d9ec3dc49f7e4b1e89d165f0d714d837e2.tar.gz";
-    sha256 = "1q1z3mrdr0cf5p4nayc1lwq56n1080mkjzny44p2yzjpp8mydj66";
-  };
+  holonixPath = (import ./nix/sources.nix).holonix;
 
   holonix = import (holonixPath) {
-    includeHolochainBinaries = false;
+    include = {
+      holochainBinaries = false;
+    };
   };
 
 in holonix.main
