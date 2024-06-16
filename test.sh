f2e149fa1bae20121d7690f55357b66df9ea9e4b
Matt Gabrenya <matt@buildyourweb.app>
2024-06-16 13:41:49 -0700

    refactor: avoid passing feature flag up by disabling all wasmer features when importing -- we only need the RuntimeError type

--- test.sh
+++ test.sh
@@ -10,6 +10,8 @@ cargo fmt
 cargo clippy
 ( cd test && cargo clippy )
 ( cd crates/guest && cargo clippy --target wasm32-unknown-unknown )
+cargo clippy --no-default-features --features wasmer_wamr
+( cd test && cargo clippy --no-default-features --features wasmer_wamr )
 
 # tests the root workspace that doesn't include any wasm code
 cargo test ${1-} -- --nocapture
@@ -19,10 +21,6 @@ cargo build --release --manifest-path test/test_wasm/Cargo.toml --target wasm32-
 
 # build wasm and run the "full" tests for wasmer_sys
 cargo test --release --manifest-path test/Cargo.toml ${1-} -- --nocapture 
-
-cargo clippy --no-default-features --features wasmer_wamr
-( cd test && cargo clippy --no-default-features --features wasmer_wamr )
-( cd crates/guest && cargo clippy --target wasm32-unknown-unknown --no-default-features --features wasmer_wamr )
 
 # build wasm and run the "full" tests for wasmer_wamr
 cargo test --release --manifest-path test/Cargo.toml --no-default-features --features wasmer_wamr ${1-} -- --nocapture
