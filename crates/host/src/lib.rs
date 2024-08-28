f0efa536ee462c5b2e42737f66d700f5576b3e94
Matt Gabrenya <matt@buildyourweb.app>
2024-08-28 11:28:20 -0700

    fix: bump hc-wasmer, feature flags working

--- crates/host/src/lib.rs
+++ crates/host/src/lib.rs
@@ -1,13 +1,19 @@
-#[cfg(all(feature = "wasmer_sys", feature = "wasmer_wamr"))]
-compile_error!("feature \"wasmer_sys\" and feature \"wasmer_wamr\" cannot be enabled at the same time");
+pub mod env;
+pub mod error;
+pub mod guest;
+pub mod module;
+/// Adapted from: https://raw.githubusercontent.com/ticki/plru/master/src/lib.rs
+/// Updated for latest stable rust.
+pub mod plru;
+pub mod prelude;
 
-#[cfg(all(
-  not(feature = "wasmer_sys"),
-  not(feature = "wasmer_wamr"),
-))]
+#[cfg(all(feature = "wasmer_sys", feature = "wasmer_wamr"))]
 compile_error!(
-  "One of: `wasmer_sys`, `wasmer_wamr` features must be enabled. Please, pick one."
+    "feature \"wasmer_sys\" and feature \"wasmer_wamr\" cannot be enabled at the same time"
 );
+
+#[cfg(all(not(feature = "wasmer_sys"), not(feature = "wasmer_wamr"),))]
+compile_error!("One of: `wasmer_sys`, `wasmer_wamr` features must be enabled. Please, pick one.");
 
 // Temporarily include a fork of wasmer from the git branch 'wamr', until it is officially released in wasmer v5
 #[cfg(feature = "wasmer_wamr")]
