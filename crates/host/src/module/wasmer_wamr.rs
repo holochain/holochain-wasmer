4fcefbd311a05b3a0850d11014580768c8ba8ed8
Matt Gabrenya <matt@buildyourweb.app>
2024-08-28 12:02:38 -0700

    fix: 'gas metering' -> 'metering'

--- crates/host/src/module/wasmer_wamr.rs
+++ crates/host/src/module/wasmer_wamr.rs
@@ -5,7 +5,7 @@ use wasmer::Engine;
 use wasmer::Module;
 
 /// Generate an engine with a wasm interpreter
-/// The interpreter used (wasm micro runtime) does not support gas metering
+/// The interpreter used (wasm micro runtime) does not support metering
 /// See tracking issue: https://github.com/bytecodealliance/wasm-micro-runtime/issues/2163
 pub fn make_engine() -> Engine {
     Engine::default()
