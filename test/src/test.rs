f39f46c95858643b363ab9777e444123a3147ade
Matt Gabrenya <matt@buildyourweb.app>
2024-08-30 10:53:38 -0700

    doc: clarifying comment

--- test/src/test.rs
+++ test/src/test.rs
@@ -1,6 +1,7 @@
 pub mod import;
 pub mod wasms;
 
+// Temporarily include a fork of wasmer from the git branch 'wamr', until it is officially released in wasmer v5
 #[cfg(feature = "wasmer_wamr")]
 extern crate hc_wasmer as wasmer;
 
