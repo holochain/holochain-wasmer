e8e1d7ba540bd9ce0a79965435fc128645cad534
Matt Gabrenya <matt@buildyourweb.app>
2024-08-30 11:13:36 -0700

    fix: include error_as_host feature, only run static tests once

--- scripts/test-wasmer_wamr.sh
+++ scripts/test-wasmer_wamr.sh
@@ -4,8 +4,5 @@ set -euxo pipefail
 export RUST_BACKTRACE=full
 export WASMER_BACKTRACE=1
 
-cargo clippy --no-default-features --features wasmer_wamr
-( cd test && cargo clippy --no-default-features --features wasmer_wamr )
-
 # build wasm and run the "full" tests for wasmer_wamr
-cargo test --release --manifest-path test/Cargo.toml --no-default-features --features wasmer_wamr ${1-} -- --nocapture
+cargo test --release --manifest-path test/Cargo.toml --no-default-features --features error_as_host,wasmer_wamr ${1-} -- --nocapture
