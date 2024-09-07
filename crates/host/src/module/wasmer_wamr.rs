fc36003d1caa2e2441f545ea374925f8db18ee78
Matt Gabrenya <matt@buildyourweb.app>
2024-09-07 13:03:06 -0700

    chore: clippy & fmt

--- crates/host/src/module/wasmer_wamr.rs
+++ crates/host/src/module/wasmer_wamr.rs
@@ -1,5 +1,6 @@
+use crate::error::PreCompiledSerilializedModuleError;
+use std::path::Path;
 use std::path::PathBuf;
-use wasmer::CompileError;
 use wasmer::DeserializeError;
 use wasmer::Engine;
 use wasmer::Module;
@@ -18,8 +19,8 @@ pub fn make_runtime_engine() -> Engine {
 /// Compile a wasm binary, serialize it with wasmer's serializtion format, and write to a file.
 /// This file can later be used for contexts where JIT compilation is not possible (iOS for example).
 pub fn write_precompiled_serialized_module_to_file(
-    wasm: &[u8],
-    path: PathBuf,
+    _wasm: &[u8],
+    _path: PathBuf,
 ) -> Result<(), PreCompiledSerilializedModuleError> {
     unimplemented!("The feature flag 'wasmer_sys' must be enabled to support compiling wasm");
 }
@@ -28,7 +29,7 @@ pub fn write_precompiled_serialized_module_to_file(
 /// Even though the `wasmer_wamr` feature flag supports deserializing a pre-compiled and serialized module,
 /// it doesn't make sense to use a pre-compiled module as it would be executed by the interpreter engine anyway.
 pub fn read_precompiled_serialized_module_from_file(
-    path: &Path,
+    _path: &Path,
 ) -> Result<Module, DeserializeError> {
     unimplemented!("The feature flag 'wasmer_sys' must be enabled to support compiling wasm");
 }
