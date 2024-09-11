f0ad4ab2e307542a3d9222ab693b539ef0cb045f
Matt Gabrenya <matt@buildyourweb.app>
2024-09-11 14:22:42 -0700

    refactor: remove unimplemented functions

--- crates/host/src/module/wasmer_wamr.rs
+++ crates/host/src/module/wasmer_wamr.rs
@@ -1,9 +1,4 @@
-use crate::error::PreCompiledSerilializedModuleError;
-use std::path::Path;
-use std::path::PathBuf;
-use wasmer::DeserializeError;
 use wasmer::Engine;
-use wasmer::Module;
 
 /// Generate an engine with a wasm interpreter
 /// The interpreter used (wasm micro runtime) does not support metering
@@ -14,22 +9,4 @@ pub fn make_engine() -> Engine {
 
 pub fn make_runtime_engine() -> Engine {
     Engine::default()
-}
-
-/// Compile a wasm binary, serialize it with wasmer's serialization format, and write it to a file.
-/// This file can later be used for contexts where JIT compilation is not possible (iOS for example).
-pub fn write_precompiled_serialized_module_to_file(
-    _wasm: &[u8],
-    _path: PathBuf,
-) -> Result<(), PreCompiledSerilializedModuleError> {
-    unimplemented!("The feature flag 'wasmer_sys' must be enabled to support compiling wasm");
-}
-
-/// Deserialize a previously precompiled and serialized module.
-/// Even though the `wasmer_wamr` feature flag supports deserializing a pre-compiled and serialized module,
-/// it doesn't make sense to use a pre-compiled module as it would be executed by the interpreter engine anyway.
-pub fn read_precompiled_serialized_module_from_file(
-    _path: &Path,
-) -> Result<Module, DeserializeError> {
-    unimplemented!("The feature flag 'wasmer_sys' must be enabled to support compiling wasm");
 }
