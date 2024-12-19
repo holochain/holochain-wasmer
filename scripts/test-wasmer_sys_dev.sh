50bd5a20f13cc50fd0523c757f09eef8f3784b5e
Matt Gabrenya <matt@buildyourweb.app>
2024-12-18 17:48:47 -0700

    chore: move building test wasms before testing root workspace

--- scripts/test-wasmer_sys_dev.sh
+++ scripts/test-wasmer_sys_dev.sh
@@ -9,11 +9,11 @@ cargo fmt --check
 cargo clippy -- --deny warnings
 ( cd crates/guest && cargo clippy --target wasm32-unknown-unknown -- --deny warnings )
 
-# tests the root workspace
-cargo test --no-default-features --features error_as_host,wasmer_sys_dev ${1-} -- --nocapture
-
 # test that test wasms build
 cargo build --release -p test_wasm --target wasm32-unknown-unknown
+
+# tests the root workspace
+cargo test --no-default-features --features error_as_host,wasmer_sys_dev ${1-} -- --nocapture
 
 # build wasm and run the "full" tests for wasmer_sys_dev
 cargo test --release -p test --no-default-features --features wasmer_sys_dev ${1-} -- --nocapture
