067fb8e511f63ff3a82212066335acd5d602b49a
Matt Gabrenya <matt@buildyourweb.app>
2024-12-18 20:49:49 -0700

    refactor: instead of nesting crates within "test" crate, move all to same level within "test-crates" dir

--- test-crates/tests/build.rs
+++ test-crates/tests/build.rs
@@ -5,16 +5,21 @@ fn main() {
 
     println!("cargo:rerun-if-changed=*");
 
-    for &m in ["test_wasm", "wasm_memory", "wasm_empty", "wasm_io"].iter() {
-        let cargo_toml = Path::new(m).join("Cargo.toml");
-
+    for &m in [
+        "test_wasm",
+        "test_wasm_memory",
+        "test_wasm_empty",
+        "test_wasm_io",
+    ]
+    .iter()
+    {
         let cargo_command = std::env::var_os("CARGO");
         let cargo_command = cargo_command.as_deref().unwrap_or_else(|| "cargo".as_ref());
 
         let status = std::process::Command::new(cargo_command)
             .arg("build")
-            .arg("--manifest-path")
-            .arg(cargo_toml)
+            .arg("-p")
+            .arg(m)
             .arg("--release")
             .arg("--target")
             .arg("wasm32-unknown-unknown")
